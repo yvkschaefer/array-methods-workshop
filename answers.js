7a1ce76b53fbc1c749b84121245de23ab3609c68
yvkschaefer <yvkschaefer@gmail.com>
2016-07-08 13:18:34 +0000

    attempt at exercise 8, started to implement skeleton of ex.9

--- answers.js
+++ answers.js
@@ -105,8 +105,9 @@ console.log(longestWord('abcdefghijklmno is bigger'));
 
 
 /*## Exercise 5
-Write a function called `countVowels` that takes a string and returns the number of vowels in the 
-string. You should use `Array.prototype.reduce` to do your work.
+Write a function called `countVowels` that takes a string and 
+returns the number of vowels in the string. You should 
+use `Array.prototype.reduce` to do your work.
 
 **Hint**: You can use `String.prototype.split` again. There is a way to use it to split a string 
 by character. Try to Google it :)
@@ -183,7 +184,7 @@ For example, starting with `[1, -10, 20, 40, 5]`, your function should return:
 ```
 */
 
-
+/*
 function highLowTwo(array){
  return array.reduce(function(init,currentNum){
        if (currentNum > init.highest) {
@@ -211,4 +212,121 @@ function highLowTwo(array){
 };
 
 console.log(highLowTwo([-10,5,17,-6]));//1:17, 2:5, 3:-6, 4:-10
+*/
 
+
+
+
+
+
+
+
+/*## Exercise 8
+Write a function called `countChars` that takes a string, and /////---> fn that takes str
+returns an object where the keys/properties are letters, and //////--->
+the value is the number of times that letter appears. //////--> sounds like .reduce?
+*/
+
+function countChars(string){ //write a function called countChars that takes a string
+   return string.reduce(function(charCount,x){ //my function returns me a reduced string based on the notion of statistics
+     if (charCount[x.letters]) {
+         charCount[x.letters] += 1;
+     }
+     else {
+         charCount[x.letters] = 1;
+     }
+      return charCount; 
+   },{})
+};
+
+console.log(countChars(['Kara Schaefer'])); //I think this might be (additionally) problematic 
+//because it asks for a string but I am giving it a string inside of an array
+
+//I think my main problem is with letters, which is undefined. I want it to be a property of my 
+//object that I'm returning
+
+
+/*
+For example, with input "hello world", the output should be:
+{
+  "h": 1,
+  "e": 1,
+  "l": 3,
+  "o": 2,
+  "w": 1,
+  "r": 1,
+  "d": 1
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+## Exercise 9
+Use `Array.prototype.reduce` to transform our array of people into an object, keyed with the 
+unique ID.
+
+The end result should look like this:
+{
+  "KeXoYg92is": {
+    "id": "KeXoYg92is",
+    "firstName": "John",
+    "lastName": "Smith",
+    "email": "john@smith.com"
+  }
+}
+This object could be useful if we are often looking up people by their unique ID.
+
+Write a function called `peopleById` that takes an array of people and returns an object where 
+each person is keyed by their unique ID.
+
+You have effectively created what we call an *index*, not unlike the one you have in your 
+phonebook.
+*/
+
+var people = [
+    {
+        'id': '8HuKIkFG6A',
+        'firstName': 'Emily',
+        'lastName': 'Haines',
+        'email': 'metric15@gmail.com'
+    },
+    {
+        'id': 'jI7Gjfl0R3',
+        'firstName': 'James',
+        'lastName': 'Shaw',
+        'email': 'rubric@gmail.com'
+    },
+    {
+        'id': 'h7KLjf9emN1',
+        'firstName': 'Beirut',
+        'lastName': 'Smith',
+        'email': 'bsmith@gmail.com'
+    }
+];
+
+
+function peopleById(arrPeople){
+    arrPeople.reduce(function(a,position){
+        if (a[position.byKey]){
+            
+        }
+    }, {byKey:sthg})
+}
+
+console.log(peopleById(people));
